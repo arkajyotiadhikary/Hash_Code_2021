6da453f0d9d4b133076720d96213b933b7529ac9
arkajyotiadhikary <arkajyotiadhikary15@gmail.com>
2021-01-07 02:09:39 +0530

    last commit jan7 2020

--- Code_Parts/UserInput.h
+++ Code_Parts/UserInput.h
@@ -7,6 +7,11 @@ class UserInput
         /* data */
     public:
         UserInput();
+
+        int no_of_variety;
+        int no_of_2_team;
+        int no_of_3_team;
+        int no_of_4_team;
 };
 
 #endif
